d8c64a1e978625856c8e218a62cdc3f02b74211a
TeufelSL <sebastian.larisch@teufel.de>
2022-11-23 20:19:29 +0100

    set correct width of a row, make sure last row has correct width if it does not completely fit in the room; remove rows setter from TilePlan

--- tests/Unit/TilePlanner/Creator/RowCreatorTest.php
+++ tests/Unit/TilePlanner/Creator/RowCreatorTest.php
@@ -10,6 +10,7 @@ use TilePlanner\TilePlanner\Creator\LastTileLengthCreatorInterface;
 use TilePlanner\TilePlanner\Creator\RowCreator;
 use TilePlanner\TilePlanner\Models\LayingOptions;
 use TilePlanner\TilePlanner\Models\Room;
+use TilePlanner\TilePlanner\Models\Row;
 use TilePlanner\TilePlanner\Models\TilePlan;
 use TilePlanner\TilePlanner\Models\TilePlanInput;
 use TilePlanner\TilePlanner\Models\Rests;
@@ -18,7 +19,9 @@ use PHPUnit\Framework\TestCase;
 
 final class RowCreatorTest extends TestCase
 {
-    public function test(): void
+    private RowCreator $creator;
+
+    protected function setUp(): void
     {
         $firstTileLengthCalculator = $this->createMock(FirstTileLengthCreatorInterface::class);
         $firstTileLengthCalculator
@@ -30,8 +33,11 @@ final class RowCreatorTest extends TestCase
             ->method('create')
             ->willReturn(Tile::create(15, 25));
 
-        $creator = new RowCreator($firstTileLengthCalculator, $lastTileLengthCalculator);
+        $this->creator = new RowCreator($firstTileLengthCalculator, $lastTileLengthCalculator);
+    }
 
+    public function test_row_has_correct_amount_of_tiles(): void
+    {
         $plan = new TilePlan();
         $rest = new Rests();
 
@@ -41,8 +47,42 @@ final class RowCreatorTest extends TestCase
             new LayingOptions(0)
         );
 
-        $actual = $creator->createRow($tileInput, $plan, $rest);
+        $actualRow = $this->creator->createRow($tileInput, $plan, $rest);
 
-        self::assertCount(5, $actual->getTiles());
+        self::assertCount(5, $actualRow->getTiles());
+    }
+
+    public function test_row_has_same_with_as_tile(): void
+    {
+        $plan = new TilePlan();
+        $rest = new Rests();
+
+        $tileInput = new TilePlanInput(
+            Room::create(200, 100),
+            Tile::create(20, 50),
+            new LayingOptions(0)
+        );
+
+        $actualRow = $this->creator->createRow($tileInput, $plan, $rest);
+
+        $this->assertEquals(20, $actualRow->getWidth());
+    }
+
+    public function test_with_of_last_row_is_less_than_tile_width(): void
+    {
+        $plan = new TilePlan();
+        $plan->addRow(new Row());
+
+        $rest = new Rests();
+
+        $tileInput = new TilePlanInput(
+            Room::create(200, 30),
+            Tile::create(20, 50),
+            new LayingOptions(0)
+        );
+
+        $actualRow = $this->creator->createRow($tileInput, $plan, $rest);
+
+        $this->assertEquals(10, $actualRow->getWidth());
     }
 }
