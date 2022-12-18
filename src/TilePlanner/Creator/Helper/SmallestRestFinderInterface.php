794a73c7f6be7c180ae59c7028f82aad23ae3dee
TeufelSL <sebastian.larisch@teufel.de>
2022-12-18 17:15:16 +0100

    fix phpstan

--- src/TilePlanner/Creator/Helper/SmallestRestFinderInterface.php
+++ src/TilePlanner/Creator/Helper/SmallestRestFinderInterface.php
@@ -6,8 +6,5 @@ use TilePlanner\TilePlanner\Models\Rest;
 
 interface SmallestRestFinderInterface
 {
-    /**
-     * @param list<Rest> $rests
-     */
     public function findSmallestRestWithMinLength(string $side, float $minWidth): ?Rest;
 }
