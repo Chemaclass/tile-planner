966a736190b386a1e4ebfab5ee2d7a44a8eacbac
TeufelSL <sebastian.larisch@teufel.de>
2022-12-18 17:33:59 +0100

    fix unit test

--- tests/Unit/TilePlanner/Creator/Helper/SmallestRestFinderTest.php
+++ tests/Unit/TilePlanner/Creator/Helper/SmallestRestFinderTest.php
@@ -6,28 +6,28 @@ namespace TilePlannerTests\Unit\TilePlanner\Creator\Helper;
 
 use PHPUnit\Framework\TestCase;
 use TilePlanner\TilePlanner\Creator\Helper\SmallestRestFinder;
-use TilePlanner\TilePlanner\Models\Rest;
+use TilePlanner\TilePlanner\Models\Rests;
+use TilePlanner\TilePlanner\TilePlannerConstants;
 
 final class SmallestRestFinderTest extends TestCase
 {
     public function test_return_null_if_rests_are_empty(): void
     {
-        $finder = new SmallestRestFinder();
+        $rests = new Rests();
+        $finder = new SmallestRestFinder($rests);
 
-        $rests = [];
-
-        $actual = $finder->findSmallestRest($rests);
+        $actual = $finder->findSmallestRestWithMinLength(TilePlannerConstants::RESTS_LEFT, 30.0);
 
         $this->assertNull($actual);
     }
 
     public function test_return_current_element_if_array_has_one_item(): void
     {
-        $finder = new SmallestRestFinder();
+        $rests = new Rests();
+        $rests->addRest(100, 30.0, TilePlannerConstants::RESTS_LEFT, 5);
+        $finder = new SmallestRestFinder($rests);
 
-        $rests = [Rest::create(100, 5)];
-
-        $actual = $finder->findSmallestRest($rests);
+        $actual = $finder->findSmallestRestWithMinLength(TilePlannerConstants::RESTS_LEFT, 30.0);
 
         $this->assertEquals(5, $actual->getNumber());
         $this->assertEquals(100, $actual->getLength());
@@ -35,15 +35,14 @@ final class SmallestRestFinderTest extends TestCase
 
     public function test_return_smallest_element_if_array_has_multiple_items(): void
     {
-        $finder = new SmallestRestFinder();
+        $rests = new Rests();
+        $rests->addRest(200, 30.0, TilePlannerConstants::RESTS_LEFT, 2);
+        $rests->addRest(100, 30.0, TilePlannerConstants::RESTS_LEFT, 5);
+        $rests->addRest(500, 30.0, TilePlannerConstants::RESTS_LEFT, 1);
 
-        $rests = [
-            Rest::create(200, 2),
-            Rest::create(100, 5),
-            Rest::create(500, 1),
-        ];
+        $finder = new SmallestRestFinder($rests);
 
-        $actual = $finder->findSmallestRest($rests);
+        $actual = $finder->findSmallestRestWithMinLength(TilePlannerConstants::RESTS_LEFT, 30.0);
 
         $this->assertEquals(5, $actual->getNumber());
         $this->assertEquals(100, $actual->getLength());
